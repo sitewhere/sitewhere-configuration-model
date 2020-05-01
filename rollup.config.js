fd274d42cf9031b247f8fcffc6b83355ad5255fe
Derek Adams <derek.adams@sitewhere.com>
2020-05-01 18:17:01 -0400

    fix: Update lib versions. Bump version.

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,25 @@
 import typescript from "rollup-plugin-typescript2";
 import pkg from './package.json';
 
+const globals = {
+  axios: "axios"
+}
+
 export default [
   {
     input: "src/index.ts",
     output: [{
       file: pkg.main,
       format: "cjs",
-      globals: {
-        axios: "axios"
-      }
+      globals: globals
     }, {
       file: pkg.module,
       format: "es",
-      globals: {
-        axios: "axios"
-      }
+      globals: globals
     }],
-    external: ["axios"],
-    plugins: [typescript()]
+    external: [
+      "axios"
+    ],
+    plugins: [typescript({ verbosity: 2, clean: true })]
   }
 ];
